0673164572594d1b45b9676fba15895a2b6ffd24
HarvielArcilla <100547307+HarvielArcilla@users.noreply.github.com>
2024-03-18 22:11:05 -0700

    powerset introduced

--- dataset/recipe_search.py
+++ dataset/recipe_search.py
@@ -1,37 +1,13 @@
 import numpy as np
 import pandas as pd
 import pickle 
-from itertools import compress
+from itertools import chain 
+from itertools import combinations
+from functools import reduce
 
 d = None
 df = None
 freq = {}
-
-def process_1K():
-    df = pd.read_csv('1K_dataset.csv')
-    df["IGS"] = df["NER"].apply(lambda x: set(x.lower().replace('[', '').replace(']', '').replace('\"', '').replace(', ', ',').split(",")))
-    d = df["IGS"].values
-    np.save('1K_IGS', d)
-    return d
-
-def process_full():
-    df = pd.read_csv('full_dataset.csv')
-    df["IGS"] = df["NER"].apply(lambda x: set(x.lower().replace('[', '').replace(']', '').replace('\"', '').replace(', ', ',').split(",")))
-    d = df["IGS"].values
-    np.save('full_IGS', d)
-    return d
-
-def process_optimized_1K():
-    df = pd.read_csv('1K_dataset.csv')
-    freq = {}
-    for i in range(df.shape[0]):
-        for ing in df["NER"][i].lower().replace('[', '').replace(']', '').replace('\"', '').replace(', ', ',').split(","):
-            if ing in freq: freq[ing].append(i)
-            else: freq[ing] = [i]
-        print(i)
-    with open('prcomputed_indices_1K.pkl', 'wb') as f:
-        pickle.dump(freq, f)
-    return df, freq
 
 def process_optimized_full():
     df = pd.read_csv('full_dataset.csv')
@@ -47,29 +23,26 @@ def process_optimized_full():
         pickle.dump(freq, f)
     return df, freq
 
-def initialize_1K():
-    df = pd.read_csv('1K_dataset.csv')
-    d = np.load('1k_IGS.npy', allow_pickle=True)
-    return df, d
+
 
 def initialize_full():
     df = pd.read_csv('full_dataset.csv')
     d = np.load('full_IGS.npy', allow_pickle=True)
     return df, d
 
-def initialize_optimized_1K():
-    df = pd.read_csv('full_dataset.csv')
-    with open('prcomputed_indices_1K.pkl', 'rb') as f:
-        freq = pickle.load(f)
-    return df, freq
+
 
 def initialize_optimized_full():
-    # df = pd.read_csv('full_dataset.csv')
     df = pd.read_pickle('full_dataset.pkl')
     with open('prcomputed_indices_full.pkl', 'rb') as f:
         freq = pickle.load(f)
     return df, freq
 
+# from itertools
+def powerset(iterable):
+    "powerset([1,2,3]) → () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+    s = list(iterable)
+    return list(chain.from_iterable(combinations(s, r) for r in range(len(s)+1)))
 
 def get_indices(ing_list):
     ingredients = set(ing_list)
@@ -91,16 +64,53 @@ def get_recipes_optimized(ing_list):
     indices = get_indices_optimized(ing_list)
     return df.loc[indices, ["title", "link"]]
 
-
-# implement lowercase for this
 if __name__ == '__main__':
-    # df, d = initialize_1K()
-    # while True:
-    #     food = input("Enter a food (Use commas w/o space): ")
-    #     print(get_recipes(food.split(",")))
-    
     # process_optimized_full()
     df, freq = initialize_optimized_full()
     while True:
         food = input("Enter a food (Use commas w/o space): ")
-        print(get_recipes_optimized(food.split(",")))
+        # print(get_recipes_optimized(food.split(",")))
+        # print(powerset(food.split(",")))
+        dfs = []
+        for i in powerset(food.split(",")):
+            if len(i) > 3:
+                dfs.append(get_recipes_optimized(i))
+        print(reduce(lambda x, y: x.merge(y, how='outer', on=['title','link']), dfs))
+
+    
+# def process_1K():
+#     df = pd.read_csv('1K_dataset.csv')
+#     df["IGS"] = df["NER"].apply(lambda x: set(x.lower().replace('[', '').replace(']', '').replace('\"', '').replace(', ', ',').split(",")))
+#     d = df["IGS"].values
+#     np.save('1K_IGS', d)
+#     return d
+
+# def process_full():
+#     df = pd.read_csv('full_dataset.csv')
+#     df["IGS"] = df["NER"].apply(lambda x: set(x.lower().replace('[', '').replace(']', '').replace('\"', '').replace(', ', ',').split(",")))
+#     d = df["IGS"].values
+#     np.save('full_IGS', d)
+#     return d
+
+# def process_optimized_1K():
+#     df = pd.read_csv('1K_dataset.csv')
+#     freq = {}
+#     for i in range(df.shape[0]):
+#         for ing in df["NER"][i].lower().replace('[', '').replace(']', '').replace('\"', '').replace(', ', ',').split(","):
+#             if ing in freq: freq[ing].append(i)
+#             else: freq[ing] = [i]
+#         print(i)
+#     with open('prcomputed_indices_1K.pkl', 'wb') as f:
+#         pickle.dump(freq, f)
+#     return df, freq
+
+# def initialize_1K():
+#     df = pd.read_csv('1K_dataset.csv')
+#     d = np.load('1k_IGS.npy', allow_pickle=True)
+#     return df, d
+
+# def initialize_optimized_1K():
+#     df = pd.read_csv('full_dataset.csv')
+#     with open('prcomputed_indices_1K.pkl', 'rb') as f:
+#         freq = pickle.load(f)
+#     return df, freq
